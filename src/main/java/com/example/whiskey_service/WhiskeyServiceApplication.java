eb8a2ce03a8ce7b4ecbad9e8955dc124200c93c7
gangsungu <ark_@naver.com>
2025-05-24 14:47:43 +0900

    Initial commit

---
+++ src/main/java/com/example/whiskey_service/WhiskeyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.whiskey_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiskeyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhiskeyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whiskey_service/WhiskeyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whiskey_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhiskeyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
