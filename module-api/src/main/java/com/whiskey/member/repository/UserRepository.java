17a3fb60086e83570b964a88d93a90dec49bb109
gangsungu <ark_@naver.com>
2025-05-29 00:04:35 +0900

    fix:user > member 변경

--- module-api/src/main/java/com/whiskey/member/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.whiskey.member.repository;
-
-import java.util.Optional;
-import com.whiskey.member.domain.member.member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<member, Long> {
-    Optional<member> findByEmail(String email);
-}
