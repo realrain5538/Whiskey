0a9572f488cb76b7ce5100f78d9ea8375c2fc388
gangsungu <ark_@naver.com>
2025-06-04 00:09:05 +0900

    fix:jpa auditing 추가

--- module-api/src/main/java/com/whiskey/WhiskeyApplication.java
+++ module-api/src/main/java/com/whiskey/WhiskeyApplication.java
@@ -1,9 +1,12 @@
 package com.whiskey;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(scanBasePackages = "com.whiskey")
+@EnableJpaAuditing
 public class WhiskeyApplication {
     public static void main(String[] args) {
         SpringApplication.run(WhiskeyApplication.class, args);
