2806ca6b726fec2bb7f23270e8c6d6d6ad1e30f0
gangsungu <ark_@naver.com>
2025-06-04 00:07:56 +0900

    update:security config 수정

--- module-api/src/main/java/com/whiskey/config/SecurityConfig.java
+++ module-api/src/main/java/com/whiskey/config/SecurityConfig.java
@@ -3,26 +3,32 @@ package com.whiskey.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests(auth -> auth
+            .csrf(AbstractHttpConfigurer::disable)
+            .formLogin(AbstractHttpConfigurer::disable)
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(
+            authorize -> authorize
                 .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/api/members/login").permitAll()
+                .requestMatchers("/api/members/signup").permitAll()
                 .anyRequest().authenticated()
-            )
-            .formLogin()  // 기본 로그인 페이지 사용
-            .and()
-            .csrf(csrf -> csrf
-                .ignoringRequestMatchers("/h2-console/**")
             )
             .headers(headers -> headers
                 .frameOptions(frame -> frame.sameOrigin())  // H2 콘솔을 위한 iframe 허용
-            );
+            );;
 
         return http.build();
     }
