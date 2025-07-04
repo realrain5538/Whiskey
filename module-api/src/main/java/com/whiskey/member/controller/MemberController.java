0e4349da8fc28cfa7af0edc4cb6a6812c1493446
gangsungu <ark_@naver.com>
2025-07-05 01:43:50 +0900

    refactor:rest api 스펙에 맞춰 url 수정

--- module-api/src/main/java/com/whiskey/member/controller/MemberController.java
+++ module-api/src/main/java/com/whiskey/member/controller/MemberController.java
@@ -3,30 +3,24 @@ package com.whiskey.member.controller;
 import com.whiskey.member.dto.MemberRegisterValue;
 import com.whiskey.member.service.MemberService;
 import com.whiskey.response.ApiResponse;
-import com.whiskey.response.enums.SuccessCode;
 import jakarta.validation.Valid;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/api")
 @RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @PostMapping("/signup")
-    @ResponseStatus(HttpStatus.OK)
-    public ApiResponse<Map<String, Object>> signup(@Valid @RequestBody MemberRegisterValue memberDto) {
+    @PostMapping("/members")
+    public ApiResponse<Void> signup(@Valid @RequestBody MemberRegisterValue memberDto) {
         memberService.signup(memberDto);
 
-        Map<String, Object> inputData = Map.of("memberName", memberDto.memberName(), "email", memberDto.email());
-        return ApiResponse.success(SuccessCode.MEMBER_REGISTERED, inputData);
+        return ApiResponse.success("회원가입이 완료되었습니다.");
     }
 }
