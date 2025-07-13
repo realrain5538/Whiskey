361ee3757a468874fdab7d0f7afa6955d387deb8
gangsungu <ark_@naver.com>
2025-07-13 19:08:10 +0900

    refactor:다중권한 가능토록 수정

--- module-domain/src/main/java/com/whiskey/domain/member/enums/MemberRole.java
+++ module-domain/src/main/java/com/whiskey/domain/member/enums/MemberRole.java
@@ -1,5 +1,10 @@
 package com.whiskey.domain.member.enums;
 
+import java.util.Collection;
+import java.util.List;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum MemberRole {
     USER("ROLE_USER"),
     ADMIN("ROLE_ADMIN");
@@ -10,7 +15,7 @@ public enum MemberRole {
         this.authority = authority;
     }
 
-    public String getAuthority() {
-        return authority;
+    public Collection<? extends GrantedAuthority> getAuthority() {
+        return List.of(new SimpleGrantedAuthority(authority));
     }
 }
